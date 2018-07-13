526abe0f80ebdbf260265fc8d1b877b3b505f594
Lauren Perchitti <laurenperchitti@gmail.com>
2018-07-13 16:21:24 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,9 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://application/vnd.github.mercy-preview+json' do |req|
+      req.params['client_id'] = ''
+      req.params['client_secret'] = ''
+      req.params['v'] = '20180713'
   end
 end
